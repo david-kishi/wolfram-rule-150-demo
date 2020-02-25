2ba8868e2c91431ebe6bf20f3eb7bc0980cfe23e
david-kishi <dansdk.blue@gmail.com>
2020-02-24 21:34:16 -0800

    Updated index. Added 3D Rule Index. Updated styles.css. Updated grid line function.

--- static/js/draw-stuff.js
+++ static/js/draw-stuff.js
@@ -5,31 +5,25 @@
 // =====================================================  draw_grid ====
 // Draw a fancy grid with major & minor lines 
 // & major row/col numbers.
-function draw_grid( rminor, rmajor, rstroke, rfill  ) 
-{
-    stroke( rstroke );
-    fill( rfill );;
+function draw_grid(rminor, rmajor, rstroke) {
+    stroke(rstroke);
     let sz = g_canvas.cell_size;
-    let width = g_canvas.wid*sz;
-    let height = g_canvas.hgt*sz
-    for ( var ix = 0; ix < width; ix += rminor )
-    {
-        let big_linep = (ix % rmajor == 0);
-        let line_wgt = 1;
-        if (big_linep) line_wgt = 2;
-        strokeWeight( line_wgt );
-        line( ix, 0, ix, height );
-        strokeWeight( 1 );
-        if ( ix % rmajor == 0 ) { text( ix, ix, 10 ); }
+    let width = g_canvas.wid * sz;
+    let height = g_canvas.hgt * sz
+    for (var ix = 0; ix < width; ix += rminor) {
+        if (ix != 0) {
+            let line_wgt = 0.25;
+            strokeWeight(line_wgt);
+            line(ix, 0, ix, height);
+            strokeWeight(1);
+        }
     }
-    for ( var iy = 0; iy < height; iy += rminor )
-    {
-        let big_linep = (iy % rmajor == 0);
-        let line_wgt = 1;
-        if (big_linep) line_wgt = 2;
-        strokeWeight( line_wgt );
-        line( 0, iy, width, iy );
-        strokeWeight( 1 );
-        if ( iy % rmajor == 0 ) { text( iy, 0, iy + 10 ); }
+    for (var iy = 0; iy < height; iy += rminor) {
+        if (iy != 0) {
+            let line_wgt = 0.25;
+            strokeWeight(line_wgt);
+            line(0, iy, width, iy);
+            strokeWeight(1);
+        }
     }
 }
