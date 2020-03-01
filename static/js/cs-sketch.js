dc53274566ec7f39017bf882e0e2843bec912a45
david-kishi <dansdk.blue@gmail.com>
2020-02-29 21:27:21 -0800

    Completed Wolfram Rule 150 Visualization. Big-O of O(N^2).

--- static/js/cs-sketch.js
+++ static/js/cs-sketch.js
@@ -1,5 +1,4 @@
 // cs-sketch.js; P5 key animation fcns.  // CF p5js.org/reference
-// Time-stamp: <2020-02-02 15:58:23 Chuck Siska>
 
 // Make global g_canvas JS 'object': a key-value 'dictionary'.
 var g_canvas = {
@@ -7,9 +6,11 @@ var g_canvas = {
     wid: 41,
     hgt: 41
 }; // JS Global var, w canvas size info.
+
 var g_frame_cnt = 0; // Setup a P5 display-frame counter, to do anim
-var g_frame_mod = 24; // Update ever 'mod' frames.
+var g_frame_mod = 1; // Update ever 'mod' frames.
 var g_stop = 0; // Go by default.
+
 // 3-D Rule Index
 var rule_index = [
     [
@@ -25,104 +26,105 @@ var rule_index = [
         [false, true]
     ]
 ]
-console.log(rule_index);
 
 function setup() // P5 Setup Fcn
 {
     let sz = g_canvas.cell_size;
     let width = sz * g_canvas.wid; // Our 'canvas' uses cells of given size, not 1x1 pixels.
     let height = sz * g_canvas.hgt;
-    createCanvas(width, height); // Make a P5 canvas.
+    createCanvas(width, height).parent("demoCanvas"); // Make a P5 canvas.
+    background(0);
     draw_grid(10, 50, 'black');
+
+    // Initiate generation 0
+    fill(765);
+    stroke(765);
+    rect((int(g_canvas.wid / 2) * g_canvas.cell_size) + 0.75, 0.75, g_canvas.cell_size - 1.5, g_canvas.cell_size - 1.5);
+
 }
 
 var g_bot = {
-    dir: 3,
-    x: 20,
-    y: 20,
-    color: 100
-}; // Dir is 0..7 clock, w 0 up.
+    x: 0,
+    y: 1,
+};
+
 var g_box = {
     t: 1,
-    hgt: 47,
+    hgt: g_canvas.hgt,
     l: 1,
-    wid: 63
-}; // Box in which bot can move.
+    wid: g_canvas.wid
+};
 
 function move_bot() {
-    let dir = (round(8 * random())) // Change direction at random; brownian motion.
-    let dx = 0;
-    let dy = 0;
-    switch (dir) { // Convert dir to x,y deltas: dir = clock w 0=Up,2=Rt,4=Dn,6=Left.
-        case 0: {
-            dy = -1;
-            break;
-        }
-        case 1: {
-            dx = 1;
-            dy = -1;
-            break;
-        }
-        case 2: {
-            dx = 1;
-            break;
-        }
-        case 3: {
-            dx = 1;
-            dy = 1;
-            break;
-        }
-        case 4: {
-            dy = 1;
-            break;
-        }
-        case 5: {
-            dx = -1;
-            dy = 1;
-            break;
-        }
-        case 6: {
-            dx = -1;
-            break;
-        }
-        case 7: {
-            dx = -1;
-            dy = -1;
-            break;
+    let dx = 1;
+
+    let x = (dx + g_bot.x + g_box.wid) % g_box.wid; // Move-x.  Ensure positive b4 mod.
+    let y = g_bot.y;
+
+    // if bot reaches end of row, increment y to move bot to next row
+    if (x == 0) {
+        y += 1;
+        if (y > g_box.hgt) { // if y is greater than box height, end
+            g_stop = !g_stop;
+        } else {
+            g_bot.y = y;
         }
     }
-    let x = (dx + g_bot.x + g_box.wid) % g_box.wid; // Move-x.  Ensure positive b4 mod.
-    let y = (dy + g_bot.y + g_box.hgt) % g_box.hgt; // Ditto y.
-    let color = 100 + (1 + g_bot.color) % 156; // Incr color in nice range.
+
     g_bot.x = x; // Update bot x.
-    g_bot.y = y;
-    g_bot.dir = dir;
-    g_bot.color = color;
-    //console.log( "bot x,y,dir,clr = " + x + "," + y + "," + dir + "," +  color );
+
 }
 
 function draw_bot() // Convert bot pox to grid pos & draw bot.
 {
     let sz = g_canvas.cell_size;
     let sz2 = sz / 2;
-    let x = 1 + g_bot.x * sz; // Set x one pixel inside the sz-by-sz cell.
-    let y = 1 + g_bot.y * sz;
-    let big = sz - 2; // Stay inside cell walls.
-    // Fill 'color': its a keystring, or a hexstring like "#5F", etc.  See P5 docs.
-    fill("#" + g_bot.color); // Concat string, auto-convert the number to string.
-    //console.log( "x,y,big = " + x + "," + y + "," + big );
-    let acolors = get(x + sz2, y + sz2); // Get cell interior pixel color [RGBA] array.
-    let pix = acolors[0] + acolors[1] + acolors[2];
-    //console.log( "acolors,pix = " + acolors + ", " + pix );
+    let x = 0.75 + g_bot.x * sz; // Set x one pixel inside the sz-by-sz cell.
+    let y = 0.75 + g_bot.y * sz;
+    let big = sz - 1.5; // Stay inside cell walls.
 
-    // (*) Here is how to detect what's at the pixel location.  See P5 docs for fancier...
-    if (0 != pix) {
+    // family of cells from prev gen; 0=white / 1=black
+    let left = me = right = 0;
+    let pix = 765; // white
+
+    // Peek into family of cells in previous generation
+    // Left
+    if (g_bot.x == 0) {
+        left = 0;
+    } else {
+        let lColors = get(x + sz2 - sz - 0.75, y + sz2 - sz - 0.75);
+        pix = lColors[0] + lColors[1] + lColors[2];
+        if (pix == 765) {
+            left = 1;
+        }
+    }
+
+    // Me
+    let mColors = get(x + sz2 - 0.75, y + sz2 - sz - 0.75);
+    pix = mColors[0] + mColors[1] + mColors[2];
+    if (pix == 765) {
+        me = 1;
+    }
+
+    // Right
+    if (g_bot.x == g_box.wid - 1) {
+        right = 0;
+    } else {
+        let rColors = get(x + sz2 + sz - 0.75, y + sz2 - sz - 0.75);
+        pix = rColors[0] + rColors[1] + rColors[2];
+        if (pix == 765) {
+            right = 1;
+        }
+    }
+
+    // Determine stroke and fill based on rule_index
+    if (rule_index[left][me][right]) {
+        fill('white');
+        stroke('white');
+    } else {
         fill(0);
         stroke(0);
-    } // Turn off color of prior bot-visited cell.
-    else {
-        stroke('white');
-    } // Else Bot visiting this cell, so color it.
+    }
 
     // Paint the cell.
     rect(x, y, big, big);
@@ -130,9 +132,8 @@ function draw_bot() // Convert bot pox to grid pos & draw bot.
 
 function draw_update() // Update our display.
 {
-    //console.log( "g_frame_cnt = " + g_frame_cnt );
-    move_bot();
     draw_bot();
+    move_bot();
 }
 
 function draw() // P5 Frame Re-draw Fcn, Called for Every Frame.
@@ -145,8 +146,4 @@ function draw() // P5 Frame Re-draw Fcn, Called for Every Frame.
 
 function keyPressed() {
     g_stop = !g_stop;
-}
-
-function mousePressed() {
-    console.log("Mouse pressed.")
 }
--- static/js/draw-stuff.js
+++ static/js/draw-stuff.js
@@ -1,18 +1,12 @@
-// Draw stuff, with P5  // CF p5js.org/reference
-// Time-stamp: <2020-02-02 14:46:00 Chuck Siska>
-// ------------------------------------------------------------
-
-// =====================================================  draw_grid ====
-// Draw a fancy grid with major & minor lines 
-// & major row/col numbers.
+// Draw a grid
 function draw_grid(rminor, rmajor, rstroke) {
     stroke(rstroke);
     let sz = g_canvas.cell_size;
     let width = g_canvas.wid * sz;
     let height = g_canvas.hgt * sz
+    let line_wgt = 0.25;
     for (var ix = 0; ix < width; ix += rminor) {
         if (ix != 0) {
-            let line_wgt = 0.25;
             strokeWeight(line_wgt);
             line(ix, 0, ix, height);
             strokeWeight(1);
@@ -20,7 +14,6 @@ function draw_grid(rminor, rmajor, rstroke) {
     }
     for (var iy = 0; iy < height; iy += rminor) {
         if (iy != 0) {
-            let line_wgt = 0.25;
             strokeWeight(line_wgt);
             line(0, iy, width, iy);
             strokeWeight(1);
